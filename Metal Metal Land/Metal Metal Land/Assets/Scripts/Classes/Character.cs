62ccfa2b80b0615d6f8c5eebd7e8ebf396cdaa32
Irish-Day-Man <paulredmond1994@gmail.com>
2018-03-16 17:32:55 +0000

    added character select related things

---
+++ Metal Metal Land/Metal Metal Land/Assets/Scripts/Classes/Character.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Collections;
+
+public class Character {
+
+    private string name;
+    private string nationality;
+    private string bio;
+    private string age;
+
+    Character(string name, string nationality, string bio, string age)
+    {
+        this.name = name;
+        this.nationality = nationality;
+        this.bio = bio;
+        this.age = age;
+
+    }//end constructor
+
+    //setters & getters
+    public void setName(string name)
+    {
+        this.name = name;
+
+    }//end setName
+
+    public string getName()
+    {
+        return this.name;
+
+    }//end getName
+
+    public void setNationality(string nationality)
+    {
+        this.nationality = nationality;
+
+    }//end setNationality
+
+    public string getNationality()
+    {
+        return nationality;
+
+    }//end string
+
+    public void setBio(string bio)
+    {
+        this.bio = bio;
+
+    }//end setBio
+
+    public string getBio()
+    {
+        return bio;
+
+    }//end getBio
+
+    public void setAge(string age)
+    {
+        this.age = age;
+
+    }//end setAge
+
+    public string getAge()
+    {
+        return age;
+
+    }//end getAge
+
+
+}
--- Metal Metal Land/Metal Metal Land/Assets/Scripts/MiscScripts/StaticScript.cs
+++ Metal Metal Land/Metal Metal Land/Assets/Scripts/MiscScripts/StaticScript.cs
@@ -1,5 +1,7 @@
 ﻿public static class StaticScript{
 
     public static string nextSceneToLoad = "";
+    public static string player1Character = "";
+    public static string player2Character = "";
 
-}
+}//end static script
