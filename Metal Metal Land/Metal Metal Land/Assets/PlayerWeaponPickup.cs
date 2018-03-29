67ba455cc25c9426c6f0a5c61bd02f2a091a83d8
Irish-Day-Man <paulredmond1994@gmail.com>
2018-03-29 18:08:52 +0100

    incorporated in weapon pickup and fire

---
+++ Metal Metal Land/Metal Metal Land/Assets/PlayerWeaponPickup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeaponPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.tag == "LiveAltar")
+        {
+            if (Input.GetKey("m"))
+            {
+                Debug.Log("Picked up weapon");
+                GameObject weaponSlot = transform.GetChild(0).gameObject;
+                weaponSlot = Instantiate(collider.GetComponent<LiveWeaponAltarScript>().getAltarWeapon(),transform.GetChild(0).transform.position,transform.rotation) as GameObject;
+                weaponSlot.transform.parent = this.transform;
+
+            }//end if
+
+        }
+
+
+    }//end onTriggerEnter
+}
