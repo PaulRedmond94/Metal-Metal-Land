7dc81da94dc651ba9fc6dbbcc5c90667c4714269
Irish-Day-Man <paulredmond1994@gmail.com>
2018-04-12 02:20:17 +0100

    modified match options scene to allow for endless mode and rounds from 10 to 50

--- Metal Metal Land/Metal Metal Land/Assets/Scripts/GUI Scripts/AutoUpdateSliderNumber.cs
+++ Metal Metal Land/Metal Metal Land/Assets/Scripts/GUI Scripts/AutoUpdateSliderNumber.cs
@@ -20,8 +20,21 @@ public class AutoUpdateSliderNumber : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        sliderVal = Mathf.RoundToInt(sliderObj.GetComponent<Slider>().value);
-        sliderTxtVal.text = "" + sliderVal;
+        //sliderVal = Mathf.RoundToInt(sliderObj.GetComponent<Slider>().value);
+        sliderVal = (int)sliderObj.GetComponent<Slider>().value;
+
+        //if round is <=50 then show the number, otherwise, the rounds will be endless
+        if (sliderVal<= 50)
+        {
+            sliderTxtVal.text = "" + sliderVal;
+
+        }//end if
+        else
+        {
+            sliderTxtVal.text = "ENDLESS!";
+
+        }
+        
 	
 	}//end update
 }
--- Metal Metal Land/Metal Metal Land/Assets/Scripts/GUI Scripts/BeginGameScript.cs
+++ Metal Metal Land/Metal Metal Land/Assets/Scripts/GUI Scripts/BeginGameScript.cs
@@ -24,8 +24,22 @@ public class BeginGameScript : MonoBehaviour {
 
     void beginGame()
     {
-        //reset player scores and tell loading manager to load up 
-        StaticScript.roundCount = System.Int32.Parse(roundCounter.GetComponent<Text>().text);
+        //reset player scores and tell loading manager to load up
+        int rounds;
+
+        //code will succesfully parse 10-50 rounds, however, a val of 51 changes the value to "endless",
+        //this causes the catch to trigger and set the rounds to max
+        try
+        {
+            rounds = int.Parse(roundCounter.GetComponent<Text>().text);
+        }
+        catch (System.FormatException formatException)
+        {
+            rounds = int.MaxValue;
+            
+        }//end catch
+
+        StaticScript.roundCount = rounds;
         StaticScript.player1Score = 0;
         StaticScript.player2Score = 0;
         StaticScript.nextSceneToLoad = "Scenes/procGenScene";
